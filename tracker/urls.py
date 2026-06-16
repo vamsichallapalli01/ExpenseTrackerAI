4544e47afe86fc0283c6f6c6c7e21b3ed625d799
vamsi babu ch <vamsichallapalli01@gmail.com>
2026-06-16 00:20:33 -0500

    Fixed login page and authentication

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
@@ -9,12 +10,16 @@ urlpatterns = [
     name="dashboard"
     ),
 
+    path('login/',
+         auth_views.LoginView.as_view(template_name='login.html'),
+         name='login'
+        ),
+
     path(
         "register/",
         views.register_view,
         name="register"
     ),
-
 
     path(
     'income/add/',
--- tracker/views.py
+++ tracker/views.py
@@ -33,6 +33,9 @@ def register_view(request):
     return render(request, "register.html", {"form": form})
 
 
+def login_view(request):
+    return render(request, 'login.html')
+
 def logout_view(request):
     logout(request)
     return redirect("login")
