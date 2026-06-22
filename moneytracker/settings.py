88f0d7ac50aea91c9e931c7247faf3a388e3b52c
vamsi babu ch <vamsichallapalli01@gmail.com>
2026-06-22 12:40:10 -0500

    Fix Render allowed hosts and csrf

--- moneytracker/settings.py
+++ moneytracker/settings.py
@@ -22,28 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-local-dev-key"
-)
+SECRET_KEY = 'django-insecure-0_z--)4rzt==v3+kw*%9xo^3t=2rm!hk($yy0@j)q(r3+=sb94'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get("DEBUG", "False").lower() == "true" else False
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "https://expensetrackerai-0pae.onrender.com",
+    "expensetrackerai-0aep.onrender.com",
+    ".onrender.com",
 ]
 
 
 CSRF_TRUSTED_ORIGINS = [
     "https://expensetrackerai-0pae.onrender.com",
 ]
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -82,17 +76,14 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-
-    'allauth.account.middleware.AccountMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'moneytracker.urls'
@@ -195,8 +186,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'vamsichallapalli01@gmail.com'
+EMAIL_HOST_PASSWORD = 'tyoy gbvi xdyo dsii'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
@@ -216,7 +207,6 @@ REST_FRAMEWORK = {
 
 }
 
-
 # Google Login Settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
@@ -229,7 +219,6 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
-
 PWA_APP_NAME = 'Expense Tracker AI'
 PWA_APP_DESCRIPTION = "AI Powered Expense Tracker"
 PWA_APP_THEME_COLOR = '#198754'
